97c422612f3e207f2fde87b8bde51e82e048ceb2
Peter <41585899+pbanigo@users.noreply.github.com>
2022-03-18 14:19:27 +0000

    Dynamic grid working

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-main.js
+let gridSize = 4;//I'd make this an input or something
+const container = document.querySelector('#container');
+container.style.display = 'none';
+function addDiv(){
+	const grid = document.createElement('div');
+	//grid.textContent = `grid`;
+	container.appendChild(grid);
+}
+function start(){
+	container.style.display = 'grid';
+	container.style.gridTemplateColumns = `repeat(${gridSize},${960/gridSize}px)`;
+	container.style.gridTemplateRows = `repeat(${gridSize},${960/gridSize}px)`;
+	document.getElementById('start').style.display = 'none';
+	for (let i = 0; i <gridSize*gridSize; i++) {
+	addDiv();
+}
+
+}
+
+
+
